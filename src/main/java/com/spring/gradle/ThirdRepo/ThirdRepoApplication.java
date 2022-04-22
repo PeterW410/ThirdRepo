9ee792bea4fdd3971220af56293c9a4e046c4da7
Peter Weigman <pweigman@manh.com>
2022-04-22 19:20:37 -0400

    initial files

---
+++ src/main/java/com/spring/gradle/ThirdRepo/ThirdRepoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gradle.ThirdRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/gradle/ThirdRepo/ThirdRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gradle.ThirdRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
